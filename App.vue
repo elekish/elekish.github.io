2c36bd26031006e4c45e876f576ffd0ec29bf34a
Ishita <144627073+elekish@users.noreply.github.com>
2023-10-25 05:02:06 +0530

    Update App.vue

--- App.vue
+++ App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <header>
+      <!-- EDIT: Replace with your name. -->
       <h1>Ishita Biswas</h1>
       <nav>
           <a href="#portfolio">Portfolio</a>
@@ -8,23 +9,25 @@
           <a href="#contact">Contact</a>
       </nav>
     </header>
-    <main v-bind:class="fadeIn">
+    <main>
       <section id="landing">
-          <p>Web Designer & Developer</p>
+          <!-- EDIT: Replace with your profession/title. -->
+          <p>Student at Jadavpur University(Junior year)</p>
       </section>
 
       <section id="portfolio">
           <h2>My Work</h2>
-          <div class="project-card">
-              <img src="path_to_project_image1.jpg" alt="Project 1" />
-              <h3>Project Title 1</h3>
-              <p>Short description of the project.</p>
-              <a href="#">View Details</a>
-          </div>
+          <!-- We'll use a component to represent each project for modularity. -->
+          <ProjectCard 
+            v-for="project in projects" 
+            :key="project.id"
+            :project="project" />
       </section>
     </main>
     <footer>
+      <!-- EDIT: Update the year and name accordingly. -->
       <p>© 2023 by Your Name. All rights reserved.</p>
+      <!-- EDIT: Update with your social media links. -->
       <div>
           <a href="#">Twitter</a>
           <a href="#">LinkedIn</a>
@@ -34,12 +37,34 @@
 </template>
 
 <script>
-import { fadeIn } from 'vue-animations'
+import { fadeIn } from 'vue-animations';
+import ProjectCard from './components/ProjectCard.vue';
 
 export default {
   name: 'App',
+  components: {
+    ProjectCard
+  },
   data() {
     return {
+      // This array should be populated with your project details.
+      // EDIT: Add your projects here.
+      projects: [
+        {
+          id: 1,
+          title: 'Project Title 1',
+          description: 'Short description of the project.',
+          imgSrc: 'path_to_project_image1.jpg',
+          detailLink: '#'
+        },{
+          id: 1,
+          title: 'Project Title 1',
+          description: 'Short description of the project.',
+          imgSrc: 'path_to_project_image1.jpg',
+          detailLink: '#'
+        }
+        
+      ],
       fadeIn: fadeIn
     }
   }
@@ -48,12 +73,4 @@ export default {
 
 <style>
 /* ... Your previous styles go here ... */
-
-/* Responsiveness */
-@media (max-width: 768px) {
-  header, main, footer {
-    padding: 20px;
-  }
-  /* Adjust styles for mobile view as needed */
-}
 </style>
